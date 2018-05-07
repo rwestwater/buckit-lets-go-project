d0b26f64abf0e2924d3a7b0215ee03a6597433dd
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-07 15:43:33 +0100

    new visit form now displaying

--- controllers/visit_controller.rb
+++ controllers/visit_controller.rb
@@ -1,23 +1,23 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative('../models/visit.rb')
+require_relative( '../models/visit.rb' )
 
 get '/visit' do
- @visits = Visit.all()
- erb (:"visit/index")
+  @visit = Visit.all()
+  erb ( :"visit/index" )
 end
 
-get '/visit/:id' do
- @visit = Visit.find(params['id'].to_i)
- erb(:"visit/show")
-end
-
-# CREATE travel
 get '/visit/new' do
   erb(:"visit/new")
 end
 
-post'/visit/new' do
-  @visit= Visit.new()
+get '/visit/:id' do
+  @visit = Visit.find(params['id'].to_i)
+  erb(:"visit/show")
+end
+
+post '/visit/new' do
+  @visit = Visit.new(params)
   @visit.save()
+  redirect to ("/visit")
 end
--- db/travel.sql
+++ db/travel.sql
@@ -18,6 +18,8 @@ CREATE TABLE cities
 CREATE TABLE visits
 (
   id SERIAL8 PRIMARY KEY,
+  going_date INT,
+  return_date INT,
   review TEXT,
   city_id INT8 REFERENCES cities(id) ON DELETE CASCADE,
   country_id INT8 REFERENCES countries(id) ON DELETE CASCADE
