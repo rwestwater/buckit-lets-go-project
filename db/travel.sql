06c65a90f599e022093329cde5fe9ab9d7734a5b
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-07 09:56:23 +0100

    db created and tables dropping

--- db/travel.sql
+++ db/travel.sql
@@ -1,17 +1,16 @@
+DROP TABLE visits;
 DROP TABLE cities;
 DROP TABLE countries;
-DROP TABLE visits;
+
+CREATE TABLE countries(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) not null
+);
 
 CREATE TABLE cities(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
-  country_id INT4 REFERENCES countries(id),
-);
-
-CREATE TABLE countries(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255) no null
-  --continent VARCHAR(255)
+  country_id INT8 REFERENCES countries(id)
 );
 
 CREATE TABLE visits(
@@ -19,6 +18,3 @@ CREATE TABLE visits(
   city_id INT8 REFERENCES cities(id),
   country_id INT8 REFERENCES countries(id)
 );
-
---countries visited
---cities visited
