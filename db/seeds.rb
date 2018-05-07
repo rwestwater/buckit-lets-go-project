3ca4a3d467c69c4ab20587ebe5e9130742fb73c5
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-07 10:06:34 +0100

    seeds populated for city and country

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
-city1 ={"name" => ""}
+require_relative( "../models/city.rb" )
+require_relative( "../models/country.rb" )
+require_relative( "../models/visit.rb" )
+require("pry-byebug")
+
+city1 = City.new({"name" => "Edinburgh"})
+city2 = City.new({"name" => "Malaga"})
+city3 = City.new({"name" => "Rome"})
+
+country1 = Country.new({"name" => "Scotland"})
+country2 = Country.new({"name" => "Spain"})
+country3 = Country.new({"name" => "Italy"})
