69f7be4f54e43de075ec2ad329e470912bd34031
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-07 09:47:54 +0100

    created classes and sql_runner

---
+++ app.rb
@@ -0,0 +1,9 @@
+require('sinatra')
+require()'sinatra/contrib/all')
+require_relative('controllers/city_controller')
+require_relative('controllers/country_controller')
+require_relative('controllers/visit_controller')
+
+get '/' do
+  erb( :index )
+end
