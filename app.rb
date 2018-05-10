57b91e78cde913c3cc54b34d9d3c2851ad8693b5
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-10 19:09:29 +0100

    added more styling

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('controllers/city_controller')
-require_relative('controllers/country_controller')
-require_relative('controllers/visit_controller')
+require_relative('controllers/cities_controller')
+require_relative('controllers/countries_controller')
+require_relative('controllers/visits_controller')
 
 get '/' do
   erb(:index)
